04c32851656e12b9cf5caa85cbeabd5aa83e348c
Ricardo Gallegos <ricardo.gallegos@usach.cl>
2024-10-01 15:06:17 -0300

    seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,12 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require 'faker'
+3.times do
+  Character.create(
+    name: Faker::Movies::HarryPotter.character,
+    location: Faker::Movies::HarryPotter.location,
+    house: Faker::Movies::HarryPotter.house
+  )
+end
